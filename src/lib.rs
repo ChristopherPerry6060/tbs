29047277ebf4a0ae4d16646630f858f03bacae8a
ChristopherPerry6060 <christopher.perry410@gmail.com>
2023-01-05 22:59:39 -0500

    Cargo(feat_attr): add drain_filter to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(is_sorted)] // used in sta test for QOL
 #![feature(is_some_and)] // used in sta EntryParser::build_packed
+#![feature(drain_filter)] // used in sta::plan::PlanBuilder
 mod rtn;
 pub mod sta;
